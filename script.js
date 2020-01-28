6e8532f46ebb06de1b63d4f59becd0b5e6e17d69
Encinia <sebastienencinia@my.whccd.edu>
2020-01-27 19:31:05 -0800

    Finished lecture activity

---
+++ script.js
@@ -0,0 +1,16 @@
+//Define a variable “myGreeting” and place a string containing a generic greeting inside of it.
+
+let myGreeting = "Hola Mundo";
+console.log(myGreeting);
+
+//Output the contents of “myGreeting” using console.log(...).
+
+const pi = 3.15; //Cannot redefine a constant
+
+//Define a new variable “myNumber” and place a number of your choice inside of it.
+
+let myNumber = 13;
+
+//Call the “min” function inside of the “Math” object on both “myNumber” and “pi” - what is the result? Why?
+
+console.log(Math.min(pi, myNumber));
